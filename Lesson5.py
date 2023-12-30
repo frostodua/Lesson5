282dc01b943eb7425ecb1ad34eaa9630dc194d25
frostodua <b.r.rostislav@gmail.com>
2023-12-31 00:09:52 +0200

    task1  in develop commit

--- Lesson5.py
+++ Lesson5.py
@@ -26,9 +26,8 @@
 # - ввести з клавіатури порядковий номер одного стовпця і потім іншого стовпця
 # і поміняти їх місцями в матрицю (аналогічно зробити з рядком)
 
+#1
 import random
-num1 = int(input("Введіть початок масиву: "))
-num2 = int(input("Введіть кінець масиву: "))
 Num_size = 10
 numbers = []
 for i in range(Num_size):
@@ -37,62 +36,96 @@ for i in range(Num_size):
     elif num1 > num2:
         numbers.append(random.randint(num2, num1))
 print(numbers)
+#
+# # Сума негативних чисел
+# count_neg_sum = 0
+# for i in numbers:
+#     if i < 0:
+#         count_neg_sum -= - i
+# print(count_neg_sum)
+#
+# # Сума парних чисел
+# count_paired_sum = 0
+# for i in numbers:
+#     if i % 2 == 0:
+#         count_paired_sum += i
+# print(count_paired_sum)
+#
+# # Сума непарних чисел
+# count_unpaired_sum = 0
+# for i in numbers:
+#     if i % 2 != 0:
+#         count_unpaired_sum += i
+# print(count_unpaired_sum)
+#
+# # Добуток елементів з індексами кратними 3
+# print(numbers[3:10:3])
+# multi = 1
+# for i in range(len(numbers)):
+#     if i % 3 == 0 and i != 0:
+#         multi *= numbers[i]
+# print(multi)
+#
+# # Добуток елементів між мінімальним та максимальним елементом
+# multi_min_max = min(numbers)*max(numbers)
+# print(multi_min_max)
+#
+# # Суму елементів, що знаходяться між першим та останнім позитивними елементами
+# for i in numbers:
+#     if i > 0:
+#         index1 = numbers.index(i)
+#         print(i)
+#         print(index1)
+#         break
+# for j in numbers[::-1]:
+#     if j > 0:
+#         index2 = numbers.index(j)
+#         print(j)
+#         print(index2)
+#         break
+# sum_el = sum(numbers[index1+1:index2])
+# print(sum_el)
 
-# Сума негативних чисел
-count_neg_sum = 0
-for i in numbers:
-    if i < 0:
-        count_neg_sum -= - i
-print(count_neg_sum)
+# #2
+# import random
+# num1 = int(input("Введіть початок масиву: "))
+# num2 = int(input("Введіть кінець масиву: "))
+# Num_size = 10
+# numbers = []
+# for i in range(Num_size):
+#     if num1 < num2:
+#         numbers.append(random.randint(num1, num2))
+#     elif num1 > num2:
+#         numbers.append(random.randint(num2, num1))
+# print(numbers)
 
-# Сума парних чисел
-count_paired_sum = 0
-for i in numbers:
-    if i % 2 == 0:
-        count_paired_sum += i
-print(count_paired_sum)
+# # ■ Створити список цілих, що містить лише парні числа з першого списку;
+# count_paired = []
+# for i in numbers:
+#     if i % 2 == 0:
+#         count_paired.append(i)
+# print(count_paired )
+#
+# # ■ Створити список цілих, що містить лише непарні числа з першого списку;
+# count_unpaired = []
+# for i in numbers:
+#     if i % 2 != 0:
+#         count_unpaired.append(i)
+# print(count_unpaired )
+#
+# # ■ Створити список цілих, що містить лише негативні числа з першого списку;
+#
+# negative = []
+# for i< in numbers:
+#     if i >
 
-# Сума непарних чисел
-count_unpaired_sum = 0
-for i in numbers:
-    if i % 2 != 0:
-        count_unpaired_sum += i
-print(count_unpaired_sum)
-
-# Добуток елементів з індексами кратними 3
-print(numbers[3:10:3])
-multi = 1
-for i in range(len(numbers)):
-    if i % 3 == 0 and i != 0:
-        multi *= numbers[i]
-print(multi)
-
-# Добуток елементів між мінімальним та максимальним елементом
-multi_min_max = min(numbers)*max(numbers)
-print(multi_min_max)
-
-# Суму елементів, що знаходяться між першим та останнім позитивними елементами
-for i in numbers:
-    if i > 0:
-        index1 = numbers.index(i)
-        print(i)
-        print(index1)
-        break
-for j in numbers[::-1]:
-    if j > 0:
-        index2 = numbers.index(j)
-        print(j)
-        print(index2)
-        break
-sum_el = sum(numbers[index1+1:index2])
-print(sum_el)
-
-
-
-
-
-
-
+# index1 = -1
+# i=0
+# while index1 < 0 and i <= Num_size:
+#     if numbers.index(i) > 0:
+#         index1 = numbers.index(i)
+#     else:
+#         i += 1
 
 
 
